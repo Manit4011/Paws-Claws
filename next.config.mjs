2856309bfdeecfc22a3b924769d0915c1287e96b
Aditya Gautam <adityagautam0601@gmail.com>
2025-06-23 01:05:23 +0530

    configfix

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,10 @@
-// next.config.mjs
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['assets.thepetnest.com'],
+    domains: [
+      'assets.thepetnest.com',
+      'dm6g3jbka53hp.cloudfront.net', // <- Add this line
+    ],
   },
 };
 
